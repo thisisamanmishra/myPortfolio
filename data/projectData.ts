8136d6485b2edcbe2572ef875f8eafb4e3ee178c
Aman Mishra <100220955+thisisamanmishra@users.noreply.github.com>
2022-12-30 17:15:03 +0530

    Update projectData.ts

--- data/projectData.ts
+++ data/projectData.ts
@@ -1,56 +1,30 @@
 export const projectsList = [
-  // {
-  //   title: 'TemplatesKart',
-  //   imageLight: '/assets/images/projects/cover/templateskart.png',
-  //   blurHash: 'L4ADc400P*Zi4Tu1y;Qo00pH#YXl',
-  //   site: 'https://templateskart.com',
-  //   gitHub: 'https://github.com/MA-Ahmad/templatesKart',
-  //   description:
-  //     'TemplatesKart provides the best Chakra UI templates. Production-ready hand crafted Projects and Components built on top of ChakraUI.',
-  //   techStack: ['React', 'Nextjs', 'ChakraUI']
-  // },
   {
     title: 'BreathitAway',
-    imageLight: '/assets/images/projects/breathitaway.png',
+    imageLight: '/public/assets/images/projects/breathitaway.png',
     blurHash: 'L4ADc400P*Zi4Tu1y;Qo00pH#YXl',
-    site: 'https://breathitaway.herokuapp.com',
+    site: 'https://breathitaway.netlify.app',
     description:
       'BreathitAway is stress removing Webpage. It consists of different types of therapy which helps the client to get relief and become tension free.',
     techStack: ['html', 'css3', 'Javascript']
   },
+   {
+    title: 'Spotify Clone',
+    imageLight: '/public/assets/images/projects/musicplayer.png',
+    blurHash: 'L4ADc400P*Zi4Tu1y;Qo00pH#YXl',
+    site: 'https://github.com/thisisamanmishra/music_player/',
+    description:
+      'A Spotify clone made using ReactJs, TailwindCSS and Restful Api',
+    techStack: ['ReactJs', 'TailwindCSS']
+  },
   {
     title: 'BloggingWEb',
     site: 'https://github.com/thisisamanmishra/BloggingWEb',
-    imageLight: '/assets/images/projects/cover/blogging.png',
+    imageLight: '/public/assets/images/projects/cover/blogging.png',
     blurHash: 'L0Aer?tjH[tPyAayj[j[00ay%xkB',
     description:
       'A Blogging app Made using Backend Technology.',
     techStack: ['mongoDb','expressJs','ejs','nodeJs']
   },
-  // {
-  //   title: 'Workof Ecommerce',
-  //   imageLight: '/assets/images/projects/cover/workof.png',
-  //   blurHash: 'L2M5%%0000EN00I:-oIp00kC?^sl',
-  //   site: 'https://workof.com/',
-  //   description:
-  //     'This is an Ecommerce furniture site. It provides a single platform to multiple vendors where they can put their products on sale.',
-  //   techStack: ['Rails', 'Javascript']
-  // },
-  // {
-  //   title: 'Souq of Qatar',
-  //   imageLight: '/assets/images/projects/cover/souq.png',
-  //   blurHash: 'L1O^uE0679I00AGT?.H{00y7p2tK',
-  //   site: 'https://www.souqofqatar.com/',
-  //   description:
-  //     'Souq of Qatar is online comparison website. This website is independent of any retailers, it displays the products of all the major stores.',
-  //   techStack: ['Rails', 'Javascript']
-  // },
-  // {
-  //   title: 'GoSave',
-  //   imageLight: '/assets/images/projects/cover/gosave.png',
-  //   blurHash: 'L09sWSxdH;s?%6axWAfk00V?%jax',
-  //   site: 'https://gosavehq.com/',
-  //   description: `The GoSave app let's you send allowances, assign chores, and approve spending all from your smartphone.`,
-  //   techStack: ['Rails']
-  // }
+
 ];
